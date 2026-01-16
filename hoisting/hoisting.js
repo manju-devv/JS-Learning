6e32cee2f596879b7339e11f0b1907685c38b94c
manju-devv <manjunathpurum@gmail.com>
2026-01-16 16:41:12 +0530

    learnt about hoisting

---
+++ hoisting/hoisting.js
@@ -0,0 +1,59 @@
+// getName();
+// console.log(x);
+// console.log(getName);
+
+
+// var x = 7;
+// function getName(){
+//     console.log("Hello JS");
+// }
+
+
+
+// // if we use arrow function then it acts as variable and gives error while executing before declaration
+// getName();
+// console.log(x);
+// console.log(getName);
+
+
+// var x = 7;
+// var getName = () => {
+//     console.log("Hello JS");
+// }
+
+
+
+
+
+
+
+// var x = 7;
+// function getName(){
+//     console.log("Hello JS");
+// }
+
+
+// getName();
+// console.log(x);
+// console.log(getName);
+
+
+
+var x = 1;
+a();
+b();
+
+console.log(x);
+
+function a(){
+    var x = 10;
+    console.log(x);
+}
+
+function b(){
+    var x = 100;
+    console.log(x);
+}
+
+
+// check call stack in the browser console to understand the flow of execution
