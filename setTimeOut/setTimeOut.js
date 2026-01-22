d5aa8901d1b4fdb46d5e85c8558f7ae149ea1ac0
manju-devv <manjunathpurum@gmail.com>
2026-01-23 00:31:32 +0530

    learnt var & let setTimeout difference

--- setTimeOut/setTimeOut.js
+++ setTimeOut/setTimeOut.js
@@ -1,7 +1,52 @@
-function x(){
-    for(var i = 1;i <= 5; i++){
+// function x(){
+//     for(var i = 1;i <= 5; i++){
+//         setTimeout(() => {
+//             console.log(i)
+//         },i*1000)
+//     }
+//     console.log("Hello World! Learning SetTimeout Function")
+// }
+
+// x();
+
+// the above code if we want to do using var we need to use IIFE(Immediately Invoked Function Expression) to create a new scope for each iteration like below
+
+// function x(){
+//     for(var i = 1;i <= 5; i++){
+//         (function(i){
+//             setTimeout(() => {
+//                 console.log(i)
+//             },i*1000)
+//         })(i)
+//     }
+//     console.log("Hello World! Learning SetTimeout Function with IIFE")
+// }
+
+// or
+function z(){
+    for(var i = 1;i<=5;i++){
+        function closureee(j){
+            setTimeout(() => {
+                console.log(j);
+            },i*1000);
+        }
+        closureee(i);
+    }
+}
+z();
+
+//using var it prints 6 five times after 1 to 5 seconds respectively because var is function scoped. and it points to 
+//the same memory location whose value is 6 after the loop ends. as it forms closure with the outer function x().
+
+
+
+function y(){
+    for(let i = 1;i <= 5; i++){
         setTimeout(() => {
             console.log(i)
         },i*1000)
     }
+    console.log("Hello World! Learning SetTimeout Function with let")
 }
+
+y();
